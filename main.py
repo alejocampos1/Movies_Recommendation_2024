e53e64602d6a81bb225531aba0fce3f55846b970
alejocampos1 <alee.1894@gmail.com>
2024-09-06 16:45:57 -0600

    PI1 - Segunda subida

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ app = FastAPI()
 async def load_data_on_startup():
     global df_cast, df_collections, df_crew, df_genres, df_movies, df_prodcompanies, df_prodcountries
     try:
-        df_cast = pd.read_csv("/ruta/a/cast.csv")
-        df_collections = pd.read_csv("/ruta/a/collections.csv")
-        df_crew = pd.read_csv("/ruta/a/crew.csv")
-        df_genres = pd.read_csv("/ruta/a/genres.csv")
-        df_movies = pd.read_csv("/ruta/a/movies.csv")
-        df_prodcompanies = pd.read_csv("/ruta/a/prodcompanies.csv")
-        df_prodcountries = pd.read_csv("/ruta/a/prodcountries.csv")
+        df_cast = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/cast.csv")
+        df_collections = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/collections.csv")
+        df_crew = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/crew.csv")
+        df_genres = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/genres.csv")
+        df_movies = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/movies.csv")
+        df_prodcompanies = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/prodcompanies.csv")
+        df_prodcountries = pd.read_csv("https://raw.githubusercontent.com/alejocampos1/Henry_PI1_Alejandro-Campos/main/Datasets/Datasets_Limpios/prodcountries.csv")
         
         print("Todos los CSV se han cargado correctamente.")
     except Exception as e:

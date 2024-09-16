780d961343f993aa1c2db9badb3bc23bf6e7d58c
alejocampos1 <alee.1894@gmail.com>
2024-09-16 00:54:29 -0600

    13 SET - CORRECCIONES

--- memoria.py
+++ memoria.py
@@ -7,7 +7,7 @@ import pandas as pd
 process = psutil.Process(os.getpid())
 
 # Cargar el DataFrame df_modelo
-df_modelo = pd.read_parquet('https://github.com/alejocampos1/Henry_PI1_Alejandro-Campos/raw/main/Datasets/matriz_features.parquet')
+df_modelo = pd.read_parquet('/Users/alejocampos/Library/Mobile Documents/com~apple~CloudDocs/SOYHENRY/LABS/PI1_FASTAPI/Datasets/matriz_features2.parquet')
 
 # Medir la memoria antes del cálculo de la similitud
 mem_before = process.memory_info().rss / (1024 ** 2)  # Convertir a MB
